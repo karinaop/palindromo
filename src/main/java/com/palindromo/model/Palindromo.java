f6efa4a2faec9a8a6ef507d9ed22966cdb81e549
Karina Pereira <karina.pereira93@hotmail.com>
2022-04-22 21:30:40 -0300

    Palindrome False

--- src/main/java/com/palindromo/model/Palindromo.java
+++ src/main/java/com/palindromo/model/Palindromo.java
@@ -4,19 +4,18 @@ public class Palindromo {
 
     public Boolean invPalindromo(String palindromo) {
         palindromo = removeCharacters(palindromo);
-        
+
         String inversePalindromo = new StringBuffer(palindromo).reverse().toString();
 
         if (palindromo.equals(inversePalindromo)) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
-        
+
     }
 
-    public String removeCharacters (String aux){
+    public String removeCharacters(String aux) {
         aux = aux.replaceAll("[^\\w\\d]", "");
         aux = aux.replaceAll("[_]", "");
         aux = aux.trim().toLowerCase();
--- src/test/java/com/palindromo/model/PalindromoTest.java
+++ src/test/java/com/palindromo/model/PalindromoTest.java
@@ -6,92 +6,130 @@ import org.junit.jupiter.api.Test;
 public class PalindromoTest {
 
     @Test
-    public void PalindromoTrue1 () {
-    
+    public void PalindromoTrue1() {
+
         Palindromo palindromo = new Palindromo();
         String word = "Rotator";
-
         assertEquals(true, palindromo.invPalindromo(word));
     }
 
     @Test
-    public void PalindromoTrue2 () {
-    
+    public void PalindromoTrue2() {
+
         Palindromo palindromo = new Palindromo();
         String word = "bob";
-
         assertEquals(true, palindromo.invPalindromo(word));
     }
 
     @Test
-    public void PalindromoTrue3 () {
-    
+    public void PalindromoTrue3() {
+
         Palindromo palindromo = new Palindromo();
         String word = "madam";
-
         assertEquals(true, palindromo.invPalindromo(word));
     }
 
     @Test
-    public void PalindromoTrue4 () {
-    
+    public void PalindromoTrue4() {
+
         Palindromo palindromo = new Palindromo();
         String word = "mAlAyAlam";
-
         assertEquals(true, palindromo.invPalindromo(word));
     }
 
     @Test
-    public void PalindromoTrue5 () {
-    
+    public void PalindromoTrue5() {
+
         Palindromo palindromo = new Palindromo();
         String word = "1";
-
         assertEquals(true, palindromo.invPalindromo(word));
     }
 
     @Test
-    public void PalindromoTrue6 () {
-    
+    public void PalindromoTrue6() {
+
         Palindromo palindromo = new Palindromo();
         String sentence = "Able was I, ere I saw Elba";
-
         assertEquals(true, palindromo.invPalindromo(sentence));
     }
 
     @Test
-    public void PalindromoTrue7 () {
-    
+    public void PalindromoTrue7() {
+
         Palindromo palindromo = new Palindromo();
         String sentence = "Madam I'm Adam";
-
         assertEquals(true, palindromo.invPalindromo(sentence));
     }
 
     @Test
-    public void PalindromoTrue8 () {
-    
+    public void PalindromoTrue8() {
+
         Palindromo palindromo = new Palindromo();
         String sentence = "Step on no pets.";
-
         assertEquals(true, palindromo.invPalindromo(sentence));
     }
 
     @Test
-    public void PalindromoTrue9 () {
-    
+    public void PalindromoTrue9() {
+
         Palindromo palindromo = new Palindromo();
         String sentence = "Top spot!";
-
         assertEquals(true, palindromo.invPalindromo(sentence));
     }
 
     @Test
-    public void PalindromoTrue10 () {
-    
+    public void PalindromoTrue10() {
+
         Palindromo palindromo = new Palindromo();
         String date = "02/02/2020";
-
         assertEquals(true, palindromo.invPalindromo(date));
+    }
+
+    @Test
+    public void PalindromoFalse1 () {
+    
+        Palindromo palindromo = new Palindromo();
+        String word = "xyz";
+        assertEquals(false, palindromo.invPalindromo(word));
+    }
+
+    @Test
+        public void PalindromoFalse2 () {
+    
+        Palindromo palindromo = new Palindromo();
+        String word1 = "elephant";        
+        assertEquals(false, palindromo.invPalindromo(word1));
+    }
+
+    @Test
+        public void PalindromoFalse3 () {
+    
+        Palindromo palindromo = new Palindromo();
+        String word2 = "Country";            
+        assertEquals(false, palindromo.invPalindromo(word2));
+    }
+
+    @Test
+        public void PalindromoFalse4 () {
+    
+        Palindromo palindromo = new Palindromo();
+        String sentence = "Top . post!";                
+        assertEquals(false, palindromo.invPalindromo(sentence));
+    }
+
+    @Test
+    public void PalindromoFalse5 () {
+    
+        Palindromo palindromo = new Palindromo();
+        String sentence1 = "Wonderful-fool!";                
+        assertEquals(false, palindromo.invPalindromo(sentence1));
+    }
+
+    @Test
+    public void PalindromoFalse6() {
+
+        Palindromo palindromo = new Palindromo();
+        String sentence2 = "Wild imagination!";
+        assertEquals(false, palindromo.invPalindromo(sentence2));
     }
 }
